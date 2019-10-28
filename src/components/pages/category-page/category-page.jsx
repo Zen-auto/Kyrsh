0f28a1627a401457d30cd79c8956dbb46d6ef6b6
yaparoff <mr.yaparoff@yandex.ru>
2019-10-28 16:33:25 +0400

    Реализовал сортировку по возрастанию/убыванию цены

--- src/components/pages/category-page/category-page.jsx
+++ src/components/pages/category-page/category-page.jsx
@@ -9,6 +9,29 @@ import './category-page.scss';
 
 class CategoryPage extends Component {
 
+  state = {
+    sortOrder: 'increase'
+  };
+
+  handleChange(e) {
+    const value = e.target.value;
+
+    this.setState({
+      sortOrder: value
+    });
+  }
+
+  sortGoods() {
+    if (this.state.sortOrder === 'decrease') {
+      // сортировка по убыванию
+      this.props.goodList.sort((prev, next) => next.price - prev.price)
+    } else {
+      // сортировка по возрастанию
+      this.props.goodList.sort((prev, next) => prev.price - next.price)
+    }
+  }
+
+
   render() {
     const { goodList, title, location } = this.props;
 
@@ -18,7 +41,14 @@ class CategoryPage extends Component {
         <div className="goods-container">
           <div className="goods-container__top">
             <h2 className="goods-container__title">{ title }</h2>
+            <select className="goods-container__sorting" onChange={ e => this.handleChange(e) } >
+              <option value="increase">Сначала дешевые</option>
+              <option value="decrease">Сначала дорогие</option>
+            </select>
           </div>
+
+          { this.sortGoods() }
+
           <div className="goods-list">
             {
               goodList.map(good => {
